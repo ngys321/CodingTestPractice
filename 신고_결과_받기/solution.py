e5e3d0fa93fd39f899b49e696519ee68b2a000b9
ngys <ngys321@gmail.com>
2022-08-03 02:06:51 +0900

    초벌 작업 완성

--- 신고_결과_받기/solution.py
+++ 신고_결과_받기/solution.py
@@ -1,38 +1,34 @@
 def solution(id_list, report, k):
 
-    id_list.append("0")
     counting_dic = {}
-    for i in  id_list:
-        counting_dic[i] = 0
-    
-    report_dic = {}
     for id in id_list:
-        report_dic[id] = "0"
+        counting_dic[id] = 0
 
-    for i in report:
-        reporter, reportee = i.split()
-        if not reportee in report_dic[reporter]:
-            report_dic[reporter] = reportee
-            counting_dic[reportee] = counting_dic[reportee] + 1
-    
-    answer = []    
-
-    id_list.pop()
+    report_set = set()
+    for report_string in report:
+        report_set.add(report_string)
 
     for id in id_list:
-        num_of_get_mails = 0
-        for reporter, reportee in report_dic.items():
-            if reporter == id and counting_dic[reportee] >= k:
-                num_of_get_mails = num_of_get_mails + 1
-        answer.append(num_of_get_mails)
-                
+        for reporter_reportee_str in report_set:
+            reporter, reportee = reporter_reportee_str.split()
+            if reporter == id:
+                counting_dic[reportee] = counting_dic[reportee] + 1
     
+    answer = []
+    for id in id_list:
+        num_of_mails_got = 0
+        for reporter_reportee_str in report_set:
+            reporter, reportee = reporter_reportee_str.split()
+            if reporter == id:
+                if counting_dic[reportee] >= k:
+                    num_of_mails_got = num_of_mails_got + 1
+        answer.append(num_of_mails_got)
     
     return answer
 
 # Codes in below are for testing
-id_list = ["A", "B", "C", "D"]
-report = ["A B", "C B", "B D", "A D", "C A"]
+id_list = ["muzi", "frodo", "apeach", "neo"]
+report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
 k = 2
 
 print (solution(id_list, report, k))
