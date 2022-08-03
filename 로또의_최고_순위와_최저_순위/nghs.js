3e3e130e6dfaff6ef822cab12c80792d323bf60d
widcellent <76591065+widcellent@users.noreply.github.com>
2022-08-03 12:35:33 +0900

    Update nghs.js

--- 로또의_최고_순위와_최저_순위/nghs.js
+++ 로또의_최고_순위와_최저_순위/nghs.js
@@ -42,3 +42,19 @@ function solution(lottos, win_nums) {
     
     return answer;
 }
+
+//블로그 풀이 참조
+// function solution(lottos, win_nums) {
+//     var answer = [];
+    
+//     const correct = lottos.filter(lotto => win_nums.includes(lotto)).length;
+    
+//     const zeros = lottos.filter(lotto => lotto === 0).length;
+//     console.log(correct, zeros);
+    
+//     let min = 7-correct >= 6 ? 6 : 7-correct;
+//     let max = min-zeros < 1 ? 1 : min-zeros;
+    
+//     answer = [max, min]
+//     return answer;
+// }
