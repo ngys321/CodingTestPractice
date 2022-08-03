0c7815dff5a13778e8f2429cf094b1ca9322e1d0
widcellent <76591065+widcellent@users.noreply.github.com>
2022-08-03 12:22:09 +0900

    Update nghs.js

--- 로또의_최고_순위와_최저_순위/nghs.js
+++ 로또의_최고_순위와_최저_순위/nghs.js
@@ -1,1 +1,44 @@
-const num = 11;
+function solution(lottos, win_nums) {
+    var answer = [];
+    var zero = 0;
+    var correctNumber =0; 
+    var leastScore = 0;
+ 
+    for(let i = 0; i <6; i++ ) {
+        if(lottos[i]===0) {
+            zero = zero+1;
+            continue;
+        }
+        for(let j = 0; j <6; j++) {
+            if(lottos[i] === win_nums[j]) {
+                correctNumber = correctNumber +1;
+                break;
+                
+            }
+        }
+    }    
+    switch (correctNumber) {
+        case 2: leastScore = 5;
+            break;
+        case 3: leastScore = 4;
+            break;
+        case 4: leastScore = 3;
+            break;
+        case 5: leastScore = 2;
+            break;
+        case 6: leastScore = 1;
+            break;
+        default: leastScore = 6;
+            
+            
+    }
+    if(zero === 6 ) {
+         answer.push(leastScore-zero+1);
+         answer.push(leastScore);
+    }else {
+        answer.push(leastScore-zero);
+        answer.push(leastScore);
+    }
+    
+    return answer;
+}
